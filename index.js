d2d25786b53cd36e7a2a79074a0bc98df30ef5c3
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-09 13:48:18 -0800

    code

--- index.js
+++ index.js
@@ -49,3 +49,17 @@
 // const b = parseInt(process.argv[3])
 
 // console.log(a + b)
+
+// console.log(5 == '5')
+
+// const fs = require('fs')
+
+// fs.writeFile('example.txt', 'Hello World!', err => {
+//   if (err) { console.log(err) }
+//   console.log('File Created!')
+// })
+
+// fs.readFile('example.txt', 'utf8', (err, data) => {
+//   if (err) { console.log(err) }
+//   console.log(data)
+// })
