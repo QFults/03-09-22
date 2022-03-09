36ec48d2e388a862776bbb7521f550d93185bc02
Quinton Fults <quinton.fults.work@gmail.com>
2022-03-09 11:09:30 -0800

    code

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello')
+// console.log('hello')
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for (let i = 0; i < nums.length; i++) {
+//   console.log(nums[i])
+// }
+
+// nums.forEach(num => {
+//   console.log(num)
+// })
+
+// const doubles = nums.map(num => {
+//   return num * 2
+// })
+
+// console.log(doubles)
+
+// const evens = nums.filter(num => {
+//   return num % 2 === 0
+// })
+
+// console.log(evens)
